07e9dbe7115f4a58f9773e7ca7feac58a20cbd31
Vitor Severo <vitorgsevero@gmail.com>
2017-10-18 19:33:40 -0200

    Lendo os arquivos

--- src/LeituraCSV.java
+++ src/LeituraCSV.java
@@ -1,15 +1,27 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.Scanner;
 
+
 public class LeituraCSV {
+	
+	public static File arquivo = new File("participantes.csv"); //colocar o arquivo na mesma pasta
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+    	InputStream is = new FileInputStream(arquivo);
+    	InputStreamReader isr =  new InputStreamReader(is);
+    	BufferedReader br = new BufferedReader(isr);
+    	String s = br.readLine();
+    	
+    	while(s!=null){
+    		
+    		System.out.print(s);
+    		s = br.readLine();
+    		
 		Scanner ler = new Scanner(System.in);
-		
-
+	
 	}
 
+}
+	
 }
