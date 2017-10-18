ac7e5f351a4c9143e6ba9fc108cb48580b51ab92
Vitor Severo <vitorgsevero@gmail.com>
2017-10-18 21:03:01 -0200

    Quebrando a linha na hora de exibir

--- src/LeituraCSV.java
+++ src/LeituraCSV.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 public class LeituraCSV {
 	
 	public static File arquivo = new File("participantes.csv"); //colocar o arquivo na mesma pasta
+	private static BufferedReader reader;
 
 	public static void main(String[] args) throws IOException {
 		
+		Scanner ler = new Scanner(System.in);
+		
     	InputStream is = new FileInputStream(arquivo);
     	InputStreamReader isr =  new InputStreamReader(is);
-    	BufferedReader br = new BufferedReader(isr);
-    	String s = br.readLine();
+    	reader = new BufferedReader(isr);
+    	String linha = reader.readLine();
     	
-    	while(s!=null){
+    	while(linha!=null){
     		
-    		System.out.print(s);
-    		s = br.readLine();
-    		
-		Scanner ler = new Scanner(System.in);
+    		System.out.println(linha);
+    		linha = reader.readLine();
+
 	
 	}
+    	
 
 }
 	
