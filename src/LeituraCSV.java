e83fef7edc0698475824d9de0eb747fc0c250994
Vitor Severo <vitorgsevero@gmail.com>
2017-10-18 00:45:09 -0200

    Primeiro Commit

---
+++ src/LeituraCSV.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class LeituraCSV {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+
+	}
+
+}
---
+++ src/Participantes.java
@@ -0,0 +1,24 @@
+
+public class Participantes {
+	
+	private String nome;
+	private String email;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
